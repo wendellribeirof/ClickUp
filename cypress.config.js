674a5e95590c03208fcd3d35660d30b051854b55
wendell ribeiro <wendell_ribeirocsc@outlook.com>
2024-01-26 18:52:19 -0300

    tese

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://app.clickup.com',
     defaultCommandTimeout: 30000,
    // supportFile: false
    setupNodeEvents(on, config) {
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,33 +1,12 @@
-import loginPage from "../support/loginPage";
-import {user_valido, user_invalido} from "../fixtures/login.json";
 
-beforeEach(() => {
-    loginPage.visit();
-});
-describe("tela de login", () => {
+describe("", () => {
 
-    context('Dado que possuo usuário válido', () => {
-        it.only('Então realizo login com sucesso', () => {
-            loginPage.fillForm();
-            loginPage.submit();
-        });
+    beforeEach(() => {
+    });
 
-        context('Dado que possuo usuário invalido', () => {
-
-            it('Então realizo login sem sucesso', () => {
-                loginPage.fillForm(user_invalido.login);
-                loginPage.submit();
-                loginPage.message();
-            });
-
-            context('Dado que esqueci minha senha', () => {
-
-                it('Então devo recuperar senha', () => {
-                    loginPage.link();
-                    loginPage.fild(user_valido.login);
-                    loginPage.send();
-                });
-            });
+    context('', () => {
+        it('', () => {
+        
         });
     });
 });
