7a16f715f3df83c76b752fb48163bd61a01e69e4
wendell ribeiro <wendell_ribeirocsc@outlook.com>
2024-01-27 03:27:26 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'hqx22j',
   e2e: {
-    baseUrl: 'https://app.clickup.com',
     defaultCommandTimeout: 30000,
    // supportFile: false
    setupNodeEvents(on, config) {
